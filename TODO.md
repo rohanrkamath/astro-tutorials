7c85e7cb6e1714bcf74275539630bb6ccccd8155
Rohan Kamath <rohankamath@Rohans-MBP.home>
2023-11-25 12:42:28 -0500

    added dark theme and greeting using interactive island

---
+++ TODO.md
@@ -0,0 +1,7 @@
+## TODO for website
+
+<ol>
+    <li> buy a domain </li>
+    <li> add this site to the Domain </li>
+    <li> add rss.xml.js for RSS Feed<a href = "https://docs.astro.build/en/tutorial/5-astro-api/4/">: site </a></li>
+</ol>
---
+++ src/pages/rss.xml.js
@@ -0,0 +1,13 @@
+/* need to develop */
+
+import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+
+export async function GET(context) {
+  return rss({
+    title: 'Astro Learner | Blog',
+    description: 'My journey learning Astro',
+    site: context.site,
+    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+    customData: `<language>en-us</language>`,
+  });
+}
